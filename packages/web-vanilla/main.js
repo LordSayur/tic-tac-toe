32b6da9e27a77aea43510a39ac917cd57143bd73
lordsayur <umar_317@hotmail.com>
2021-11-21 17:16:54 +0800

    🎉 web-vanilla: init package

---
+++ packages/web-vanilla/main.js
@@ -0,0 +1,77 @@
+import "./style.css";
+import { Game } from "@tic-tac-toe/core";
+
+const game = new Game();
+
+document.addEventListener("DOMContentLoaded", () => {
+  const $startBtn = document.querySelector("#start");
+  const $undoBtn = document.querySelector("#undo");
+  const $redoBtn = document.querySelector("#redo");
+  const $replayBtn = document.querySelector("#replay");
+  const $board = document.querySelector("#board");
+  const $winnerMessage = document.querySelector("#winner-message");
+
+  $startBtn.addEventListener("click", startGame);
+  $undoBtn.addEventListener("click", () => {
+    game.undo();
+    drawBoard(game.currentState, $board);
+    setWinnerMessage("");
+    check();
+  });
+  $redoBtn.addEventListener("click", () => {
+    game.redo();
+    drawBoard(game.currentState, $board);
+    check();
+  });
+  $replayBtn.addEventListener("click", () => {
+    game.replay(() => drawBoard(game.currentState, $board));
+  });
+
+  startGame();
+
+  function startGame() {
+    game.start();
+    drawBoard(game.currentState, $board);
+    setWinnerMessage("");
+  }
+
+  function drawBoard(currentBoard, board) {
+    board.innerHTML = "";
+    currentBoard.forEach((rows, rowIndex) => {
+      const $row = document.createElement("div");
+      $row.classList.add("row");
+
+      rows.forEach((col, colIndex) => {
+        const $col = document.createElement("div");
+        $col.classList.add("col");
+        $col.setAttribute("data-row-index", rowIndex);
+        $col.setAttribute("data-col-index", colIndex);
+        $col.setAttribute("data-test", `cell-${rowIndex}-${colIndex}`);
+        $col.addEventListener("click", makeMove);
+        $col.appendChild(document.createTextNode(col));
+        $row.appendChild($col);
+      });
+      board.appendChild($row);
+    });
+  }
+
+  function makeMove(e) {
+    if (game.winner) return;
+
+    const $cell = e.target;
+    const row = $cell.getAttribute("data-row-index");
+    const col = $cell.getAttribute("data-col-index");
+    game.makeMove({ row, col });
+    drawBoard(game.currentState, $board);
+    check();
+  }
+  function check() {
+    game.check();
+    if (game.winner) setWinnerMessage(`The winner is ${game.winner}!`);
+    if (!game.winner && !game.currentState.flat().includes("-"))
+      setWinnerMessage("Its a tie!");
+  }
+  function setWinnerMessage(message) {
+    $winnerMessage.innerHTML = message;
+  }
+});
