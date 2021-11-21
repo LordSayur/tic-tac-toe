c4905ec45231aa7e6c096ffb551f1a5cdaa88b30
lordsayur <umar_317@hotmail.com>
2021-11-21 16:55:01 +0800

    ✅ core: add utility tests

---
+++ packages/core/utility.spec.js
@@ -0,0 +1,23 @@
+import { EMPTY_STATE } from ".";
+import { cloneState } from "./utility";
+
+describe("> Utility", () => {
+  describe(">> cloneState", () => {
+    it("should return cloned copy of passed state", () => {
+      const clonedState = cloneState(EMPTY_STATE);
+
+      expect(clonedState).toEqual(EMPTY_STATE);
+    });
+    it("should preserve passed state on mutating cloned state", () => {
+      const clonedState = cloneState(EMPTY_STATE);
+
+      clonedState[0][0] = "x";
+
+      expect(EMPTY_STATE).toEqual([
+        ["-", "-", "-"],
+        ["-", "-", "-"],
+        ["-", "-", "-"],
+      ]);
+    });
+  });
+});
