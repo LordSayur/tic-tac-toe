cdabc6aa823018aacb3a6cf9319b0faa14c914fa
lordsayur <umar_317@hotmail.com>
2021-11-22 20:30:38 +0800

    ✨web-vue-3: create tic-tac-toe game

--- packages/web-vue-3/src/App.vue
+++ packages/web-vue-3/src/App.vue
@@ -1,21 +1,23 @@
 <script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import TicTacToe from "./components/TicTacToe.vue";
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <TicTacToe />
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+html,
+body {
+  background-color: darkslategrey;
+  height: 100%;
+  margin: 0;
+}
+main {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
 }
 </style>
---
+++ packages/web-vue-3/src/components/TicTacToe.vue
@@ -0,0 +1,103 @@
+<script setup>
+import { Game } from "@tic-tac-toe/core";
+import { ref } from "@vue/reactivity";
+
+const game = new Game();
+
+game.start();
+
+const states = ref(game.currentState);
+const makeMove = ({ row, col }) => {
+  if (game.winner) return;
+  game.makeMove({ row, col });
+  states.value = game.currentState;
+  game.check();
+};
+const redo = () => {
+  game.redo();
+  states.value = game.currentState;
+  game.check();
+};
+const undo = () => {
+  game.undo();
+  states.value = game.currentState;
+  game.check();
+};
+const start = () => {
+  game.start();
+  states.value = game.currentState;
+};
+const replay = () => {
+  game.replay(() => (states.value = game.currentState));
+};
+</script>
+
+<template>
+  <h1>Tic Tac Toe</h1>
+  <div id="board">
+    <div v-for="(row, rowIndex) in states" key="rowIndex" class="row">
+      <div
+        v-for="(col, colIndex) in row"
+        class="col"
+        :data-test="`cell-${rowIndex}-${colIndex}`"
+        @click="makeMove({ row: rowIndex, col: colIndex })"
+      >
+        {{ col }}
+      </div>
+    </div>
+  </div>
+  <div id="buttons">
+    <button id="start" data-test="start" type="button" @click="start">
+      Start
+    </button>
+    <button id="undo" data-test="undo" type="button" @click="undo">Undo</button>
+    <button id="redo" data-test="redo" type="button" @click="redo">Redo</button>
+    <button id="replay" data-test="replay" type="button" @click="replay">
+      Replay
+    </button>
+  </div>
+  <div id="winner-message" data-test="winner-message" v-show="game.winner">
+    The winner is {{ game.winner }}!
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  text-align: center;
+  color: wheat;
+}
+button {
+  margin: 0.1rem;
+  padding: 0.5rem;
+  border: none;
+  background-color: wheat;
+}
+button:hover {
+  color: goldenrod;
+}
+#buttons {
+  padding: 1rem;
+}
+#winner-message {
+  color: wheat;
+}
+#board {
+  margin: 2rem;
+}
+.row {
+  display: flex;
+  justify-content: center;
+}
+.col {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border: 1px solid wheat;
+  height: 50px;
+  width: 50px;
+  color: wheat;
+}
+.col:hover {
+  color: goldenrod;
+}
+</style>
